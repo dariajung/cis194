fc65ef71c9505576d3e2f47dcaa7fc8becd879d4
Daria Jung <djj2115cu@gmail.com>
2014-04-29 19:13:45 -0400

    implement tower of hanoi, three pegs, x number of disks

--- 01/hanoi.hs
+++ 01/hanoi.hs
@@ -1,5 +1,6 @@
 type Peg = String
 type Move = (Peg, Peg)
 hanoi :: Integer -> Peg -> Peg -> Peg -> [Move]
-hanoi disk source dest spare = 
+hanoi 0 source dest spare = [(source, dest)]
+hanoi disk source dest spare = (hanoi (disk - 1) source spare dest) ++ (hanoi 0 source dest spare) ++ (hanoi (disk - 1) spare dest source)
    
