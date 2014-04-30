a86290873c4734b07493dc24cb911e48821ba063
Daria Jung <djj2115cu@gmail.com>
2014-04-30 12:31:53 -0400

    write guard to get MessageType

--- 02/LogAnalysis.hs
+++ 02/LogAnalysis.hs
@@ -1,4 +1,15 @@
 {-# OPTIONS_GHC -Wall #-}
-module Log Analysis where
+module LogAnalysis where
 
 import Log
+
+--parseMessage :: String -> LogMessage
+parseMessage msg =
+    let msgType = getMsgType $ words msg
+    in words msg 
+
+getMsgType :: [String] -> MessageType
+getMsgType (x:y:zs)
+    | x == "I"  = Info
+    | x == "W"  = Warning
+    | x == "E"  = Error (read y :: Int)
