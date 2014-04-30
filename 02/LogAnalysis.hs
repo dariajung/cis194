719d93621029c37e5b6fd903bcfbc6863e845be5
Daria Jung <djj2115cu@gmail.com>
2014-04-30 14:38:17 -0400

    parseMessage implemented

--- 02/LogAnalysis.hs
+++ 02/LogAnalysis.hs
@@ -3,13 +3,15 @@ module LogAnalysis where
 
 import Log
 
---parseMessage :: String -> LogMessage
-parseMessage msg =
-    let msgType = getMsgType $ words msg
-    in msgType
+parseMessage :: String -> LogMessage
+parseMessage msg
+        | length first == 1     = getMsg $ words msg 
+        | otherwise             = Unknown msg
+    where first = head $ words msg
 
-getMsgType :: [String] -> MessageType
-getMsgType (x:ys)
-    | x == "I"  = Info
-    | x == "W"  = Warning
-    | x == "E"  = Error (read $ head ys :: Int)
+getMsg :: [String] -> LogMessage
+getMsg (x:y:zs)
+    | x == "I"  = LogMessage Info (read $ y :: Int) (unwords zs)
+    | x == "W"  = LogMessage Warning (read $ y :: Int) (unwords zs)
+    | x == "E"  = LogMessage (Error (read $ y :: Int)) (read $ head zs :: Int) (unwords $ drop 1 zs)
+
