7d1397574f975d3076c98fd090787a04fe90928b
Daria Jung <djj2115cu@gmail.com>
2014-05-05 13:15:31 -0400

    refactor credit.hs -- hacker school interview

--- 01/credit.hs
+++ 01/credit.hs
@@ -1,16 +1,23 @@
 -- Validation algorithm for credit cards
-
-validate :: [Int] -> Int
-validate digits =
-   let doubled = reverse (everyOther (reverse digits))
-   in (sum $ concat $ map digs doubled) `mod` 10
-
-everyOther :: [Int] -> [Int]
+ 
+validate :: Integer -> Bool
+validate digits
+    | total `mod` 10 == 0   = True
+    | otherwise             = False
+    where doubled = everyOther (reverse (getDigits digits))
+          total = sum $ concat $ map getDigits doubled
+ 
+everyOther :: [Integer] -> [Integer]
 everyOther [] = []
 everyOther [a] = [a]
 everyOther (x:y:zs) = x : y * 2 : everyOther zs
+ 
+getDigits :: Integer -> [Integer]
+getDigits 0 = [0]
+getDigits x = getDigits (x `div` 10) ++ [x `mod` 10]
 
-digs :: Int -> [Int]
-digs 0 = []
-digs x = digs (x `div` 10) ++ [x `mod` 10]
+acceptInput :: String -> Integer
+acceptInput x = read (filter (`elem` ['0'..'9']) x) :: Integer
+
+
 
