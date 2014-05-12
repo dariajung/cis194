21b98acbc6d66f1848baa0d203aefb126b3ab026
Daria Jung <djj2115cu@gmail.com>
2014-05-12 14:53:52 -0400

    localMaxima type signature

--- 03/golf.hs
+++ 03/golf.hs
@@ -6,3 +6,5 @@ skips xs = map (every xs) [1..(length xs)]
 every xs n = case drop (n-1) xs of
               (y:ys) -> y : every ys n 
               [] -> []
+
+localMaxima :: [Integer] -> [Integer]
