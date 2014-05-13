0b7142c1df0bf9b7728b956c90deaa6013bead2c
Daria Jung <djj2115cu@gmail.com>
2014-05-13 17:28:06 -0400

    print one line of occuring elements

--- 03/golf.hs
+++ 03/golf.hs
@@ -14,12 +14,16 @@ localMaxima (x:list@(y:z:zs))
     | otherwise             = localMaxima list 
 localMaxima (_:x) = []
 
---histogram :: [Integer] -> String
-histogram xs
-            | 
-            where freq@(y:ys) = map (\x -> instances x xs) [0..9]
+histogram :: [Integer] -> String
+histogram xs =
+             let freq@(y:ys) = map (\x -> instances x xs) [0..9]
+             in (draw freq ++ "\n") ++ "==========\n0123456789\n"
 
 -- maybe a draw function?
+draw [] = ""
+draw list@(x:xs)
+    | x > 0   = "*" ++ draw xs
+    | otherwise  = " " ++ draw xs
 
 instances x [] = 0 
 instances x (y:ys) 
