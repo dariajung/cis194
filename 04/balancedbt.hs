8d0f113926bbb7450a5d12ca4f0adb40396d380e
Daria Jung <djj2115cu@gmail.com>
2014-05-20 18:53:06 -0400

    add type signature to foldtree

--- 04/balancedbt.hs
+++ 04/balancedbt.hs
@@ -1,3 +1,8 @@
 data Tree a = Leaf
     | Node Integer (Tree a) a (Tree a)
     deriving (Show, Eq)
+
+-- break this up into three parts:
+-- foldTree, insertNode, and height (of tree)
+foldTree :: [a] -> Tree a
+
