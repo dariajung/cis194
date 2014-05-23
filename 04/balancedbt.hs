49423fdb52f6fbd6ba844ea2d50ccd5c22e0cff0
Daria Jung <djj2115cu@gmail.com>
2014-05-23 19:53:46 -0400

    add insertion of nodes into binary tree

--- 04/balancedbt.hs
+++ 04/balancedbt.hs
@@ -7,7 +7,12 @@ data Tree a = Leaf
 -- foldTree :: [a] -> Tree a
 
 insert a (Node n left current right)
-    |
+    | h1 > h2   = Node n left current (insert a right)
+    | h1 < h2   = Node n (insert a left) current right
+    | otherwise = Node (h + 1) (insert a left) current right
+    where h1 = height left
+          h2 = height right
+          h = height (insert a left)
 
 height :: Tree a -> Integer
 height Leaf = 0
