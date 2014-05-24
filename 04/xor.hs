31b382b1ab2df3548579a9600c8505c71523bd96
Daria Jung <djj2115cu@gmail.com>
2014-05-23 21:20:28 -0400

    use . instead of $

--- 04/xor.hs
+++ 04/xor.hs
@@ -1,6 +1,5 @@
---xor :: [Bool] -> Bool
--- this gives me an error, not sure why yet
-xor = odd $ foldl (\acc x -> if x then acc + 1 else acc) 0
+xor :: [Bool] -> Bool
+xor = odd . foldl (\acc x -> if x then acc + 1 else acc) 0
        
  
 
