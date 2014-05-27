41e83d09e8dc3b1049c029c0bb4ad561bf35274e
Daria Jung <djj2115cu@gmail.com>
2014-05-27 16:29:49 -0400

    implement sieve of sundaram

--- 04/sieve.hs
+++ 04/sieve.hs
@@ -1,3 +1,7 @@
 -- Sieve of Sunaram
 sieveSundaram :: Integer -> [Integer]
-sieveSundaram
+sieveSundaram n =
+            let del = delete n in
+            2:[2*x + 1 | x <- [1..n], not (x `elem` del) ]
+
+delete n = [i + j + 2*i*j | i <- [1..n], j <- [1..n]]
