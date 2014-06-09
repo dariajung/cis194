d16e21e3f2709a0e932b29fb7508c46108b03b10
Daria Jung <djj2115cu@gmail.com>
2014-06-09 16:15:10 -0400

    add instances of expr for integer, bool, minmax, and mod7

--- 05/Calc.hs
+++ 05/Calc.hs
@@ -29,3 +29,35 @@ instance Expr ExprT where
     lit x = Lit x
     add x y = Add x y
     mul x y = Mul x y
+
+instance Expr Integer where
+    lit x = x
+    add x y = x + y
+    mul x y = x * y
+
+instance Expr Bool where
+    lit x = if x >= 0 then True else False
+    add x y = x || y
+    mul x y = x && y
+
+newtype MinMax = MinMax Integer deriving (Eq, Show)
+newtype Mod7 = Mod7 Integer deriving (Eq, Show)
+
+instance Expr MinMax where
+    lit x = MinMax x
+    add a@(MinMax x) b@(MinMax y) = if max x y == x then a else b
+    mul a@(MinMax x) b@(MinMax y) = if min x y == x then a else b
+
+instance Expr Mod7 where
+    lit x = Mod7 (x `mod` 7)
+    add (Mod7 x) (Mod7 y) = Mod7 ((x + y) `mod` 7)
+    mul (Mod7 x) (Mod7 y) = Mod7 ((x * y) `mod` 7)
+
+testExp :: Expr a => Maybe a
+testExp = parseExp lit add mul "(3 * -4) + 5"
+
+testInteger = testExp :: Maybe Integer
+testBool = testExp :: Maybe Bool
+testMM = testExp :: Maybe MinMax
+testSat = testExp :: Maybe Mod7
+
