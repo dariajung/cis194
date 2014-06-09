aadaa620bb6ebb778522123bbb183a97d875be2c
Daria Jung <djj2115cu@gmail.com>
2014-06-09 14:29:49 -0400

    get rid of expression@

--- 05/Calc.hs
+++ 05/Calc.hs
@@ -4,16 +4,15 @@ import ExprT
 import Parser
 
 evalMaybe :: Maybe ExprT -> Maybe Integer
-evalMaybe (Just expression@(Lit x)) = Just x
-evalMaybe (Just expression@(Add x y)) = (Just (eval x + eval y))
-evalMaybe (Just expression@(Mul x y)) = (Just (eval x * eval y))
+evalMaybe (Just (Lit x)) = Just x
+evalMaybe (Just (Add x y)) = (Just (eval x + eval y))
+evalMaybe (Just (Mul x y)) = (Just (eval x * eval y))
 evalMaybe Nothing = Nothing
 
 eval :: ExprT -> Integer
-eval expression@(Lit x) = x
-eval expression@(Add x y) = eval x + eval y
-eval expression@(Mul x y) = eval x * eval y
-
+eval (Lit x) = x
+eval (Add x y) = eval x + eval y
+eval (Mul x y) = eval x * eval y
 
 evalStr :: String -> Maybe Integer
 evalStr str = evalMaybe (parseExp Lit Add Mul str)
