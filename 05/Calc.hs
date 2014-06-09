14b454eec1339f5f9d4fb8fd9529da138093f208
Daria Jung <djj2115cu@gmail.com>
2014-06-09 14:26:15 -0400

    use parseExpg

--- 05/Calc.hs
+++ 05/Calc.hs
@@ -1,8 +1,19 @@
 module Calc where
 
 import ExprT
+import Parser
+
+evalMaybe :: Maybe ExprT -> Maybe Integer
+evalMaybe (Just expression@(Lit x)) = Just x
+evalMaybe (Just expression@(Add x y)) = (Just (eval x + eval y))
+evalMaybe (Just expression@(Mul x y)) = (Just (eval x * eval y))
+evalMaybe Nothing = Nothing
 
 eval :: ExprT -> Integer
 eval expression@(Lit x) = x
 eval expression@(Add x y) = eval x + eval y
 eval expression@(Mul x y) = eval x * eval y
+
+
+evalStr :: String -> Maybe Integer
+evalStr str = evalMaybe (parseExp Lit Add Mul str)
