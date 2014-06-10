2acc97153e6b85b7ae27ad86e8a8fdcffc9f1953
Daria Jung <djj2115cu@gmail.com>
2014-06-09 20:45:49 -0400

    memoized fibonacci

--- 06/Fibonacci.hs
+++ 06/Fibonacci.hs
@@ -5,3 +5,13 @@ fib n = fib (n - 1) + fib (n - 2)
 
 fibs1 :: [Integer]
 fibs1 = map fib [0..]
+
+fibs2 :: [Integer]
+fibs2 = map memoized_fib [0..]
+
+memoized_fib :: Int -> Integer
+memoized_fib =
+    let fib 0 = 0
+        fib 1 = 1
+        fib n = memoized_fib (n - 2) + memoized_fib (n - 1)
+    in  (map fib [0 ..] !!)
