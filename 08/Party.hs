a83cdd263e49f99ba32e0a20fda9dae70d36c6f2
Daria Jung <djj2115cu@gmail.com>
2014-06-12 18:40:51 -0400

    implement more fun

--- 08/Party.hs
+++ 08/Party.hs
@@ -4,3 +4,9 @@ import Employee
 
 glCons :: Employee -> GuestList -> GuestList
 glCons person@(Emp { empFun = y } ) gl@(GL list fun) = GL (person : list) (y + fun)
+
+moreFun :: GuestList -> GuestList -> GuestList
+moreFun l@(GL e f) l'@(GL e' f') 
+    | f > f'    = l
+    | f < f'    = l'
+    | otherwise = l
