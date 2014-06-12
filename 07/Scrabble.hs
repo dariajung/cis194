0f75115292936b3cad6c18b91ab608a5b0f03bcd
Daria Jung <djj2115cu@gmail.com>
2014-06-12 14:13:06 -0400

    implement scrabble data type and score functioin

---
+++ 07/Scrabble.hs
@@ -0,0 +1,25 @@
+{-# LANGUAGE GeneralizedNewtypeDeriving, FlexibleInstances #-}
+
+module Scrabble where
+
+import Data.Monoid
+
+newtype Score = Score Int
+    deriving (Eq, Ord, Show, Num)
+
+instance Monoid Score where
+  mempty  = Score 0
+  mappend = (+)
+
+score :: Char -> Score
+score n 
+    | n `elem` "aeiounlrstAEIOUNLRST"   = Score 1
+    | n `elem` "dD"                     = Score 2
+    | n `elem` "bcmpBCMP"               = Score 3
+    | n `elem` "fvwyFVWY"               = Score 4
+    | n `elem` "kK"                     = Score 5
+    | n `elem` "jxJX"                   = Score 8
+    | n `elem` "qzQZ"                   = Score 10
+    | otherwise                         = Score 0
+
+--scoreString :: String -> Score
