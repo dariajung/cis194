a08e4984e57f95d7c330465835df22bca54e3521
Daria Jung <djj2115cu@gmail.com>
2014-06-13 16:45:38 -0400

    fix non-exhaustive pattern in nextLevel

--- 08/Party.hs
+++ 08/Party.hs
@@ -9,14 +9,13 @@ glCons :: Employee -> GuestList -> GuestList
 glCons person@(Emp { empFun = y } ) gl@(GL list fun) = GL (person : list) (y + fun)
 
 instance Monoid GuestList where 
-  mempty = GL [] 0
-  mappend (GL emp fun) (GL emp2 fun2) = GL (emp ++ emp2) (fun + fun2) 
+    mempty = GL [] 0
+    mappend (GL emp fun) (GL emp2 fun2) = GL (emp ++ emp2) (fun + fun2)
 
 moreFun :: GuestList -> GuestList -> GuestList
 moreFun l@(GL e f) l'@(GL e' f') 
     | f > f'    = l
     | otherwise = l'
-
 
 -- already defined in Data.Tree but inserting
 -- so I can refer to the data type definition:
@@ -26,8 +25,8 @@ moreFun l@(GL e f) l'@(GL e' f')
 --      subForest :: [Tree a] -- zero or more child trees
 -- }
 
-treeFold :: (b -> a -> b) -> b -> Tree a -> b
-treeFold f accum (Node { rootLabel = x, subForest = y }) = foldl (treeFold f) (accum `f` x) y
+treeFold :: (a -> [b] -> b) -> Tree a -> b
+treeFold f tree = f (rootLabel tree) (map (treeFold f) (subForest tree))
 
 nextLevel :: Employee -> [(GuestList, GuestList)] -> (GuestList, GuestList)
 nextLevel boss list =
@@ -35,5 +34,13 @@ nextLevel boss list =
         withoutBoss = without list
     in (withBoss, withoutBoss)
 
+with :: [(GuestList, GuestList)] -> GuestList
 with ((l, l'):xs) = l' `mappend` with xs
-without ((l, l'):xs) = (l `moreFun` l') `mappend` without xs
+with [] = GL [] 0
+
+without :: [(GuestList, GuestList)] -> GuestList
+without ((l, l'):xs) = (moreFun l l') `mappend` without xs
+without [] = GL [] 0
+
+maxFun :: Tree Employee -> GuestList
+maxFun = uncurry moreFun . treeFold nextLevel
